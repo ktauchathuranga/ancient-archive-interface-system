c493888ee4c78a955a4e4726e9fa23c014dc3401
Ashen Chathuranga <ktauchathuranga@gmail.com>
2024-03-31 11:42:16 +0530

    change images
    added vector images. hence hard to find good qualty png imgages in sri lankas historical places

--- assets/js/home-main.js
+++ assets/js/home-main.js
@@ -1,7 +1,7 @@
-let cloud = document.getElementById("cloud");
-let sun = document.getElementById("sun");
-let sigiriya = document.getElementById("sigiriya");
-let mount = document.getElementById("mount");
+let star = document.getElementById("star");
+let moon = document.getElementById("moon");
+let mounb = document.getElementById("moun-back");
+let mounf = document.getElementById("moun-front");
 let text = document.getElementById("text");
 let btn = document.getElementById("btn");
 let header = document.querySelector('header');
@@ -9,10 +9,10 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', function() {
     let value = this.window.scrollY;
     
-    sun.style.top = value * 0.25 + 'px';
-    cloud.style.left = value * 1.05 + 'px';
-    sigiriya.style.top = value * 0.5 + 'px';
-    mount.style.left = value * 0 + 'px';
+    star.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mounb.style.top = value * 0.5 + 'px';
+    mounf.style.left = value * 0 + 'px';
     text.style.marginRight = value * 4 + 'px';
     text.style.marginTop = value * 1.5 + 'px';
     btn.style.marginTop = value * 1.5 + 'px';
