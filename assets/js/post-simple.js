14e77b902ba53f56e4559098cbc0e22a717ab9e9
Ishan Sivmal <ishansivmal@gmail.com>
2024-03-31 12:59:27 +0530

    post besic structure
    added css  and jequary expane option

---
+++ assets/js/post-simple.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.btns11').click(function(){
+        $('.s1p1').animate({
+            height:'toggle'
+        })
+       
+    })
+
+    $('.btns22').click(function(){
+        $('.s2p2').animate({
+            height:'toggle'
+        })
+    })
+
+    $('.btns33').click(function(){
+        $('.s3p3').animate({
+            height:'toggle'
+        })
+    })
+})
