40d454924d2244ce1f08759837603a4841596154
Ashen Chathuranga <ktauchathuranga@gmail.com>
2024-04-01 12:52:48 +0530

    add new loading method

--- assets/js/jq-loader.js
+++ assets/js/jq-loader.js
@@ -1,5 +1,4 @@
-(function() {
-    var script = document.createElement('script');
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js';
-    document.head.appendChild(script);
-  })();
+var script = document.createElement('script'); 
+ 
+script.src = '//code.jquery.com/jquery-3.7.1.min.js'; 
+document.getElementsByTagName('head')[0].appendChild(script);
