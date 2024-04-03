522e17518a838e9d2c3f5efd6f330a576e74f413
Ashen Chathuranga <ktauchathuranga@gmail.com>
2024-04-03 22:28:09 +0530

    fix home not showing when intial load
    pain, just pain :(

--- assets/js/header-footer-active.js
+++ assets/js/header-footer-active.js
@@ -1,5 +1,5 @@
 var currentPage = window.location.pathname.split('/').pop();
-    
+
 console.log("Current Page URL: " + currentPage);
 
 var headerLinks = document.querySelectorAll('header ul li');
@@ -9,7 +9,8 @@ headerLinks.forEach(function(link) {
     if (currentPage === linkClass + '.html') {
         console.log("Match found for class: " + linkClass);
         link.querySelector('a').classList.add('active');
-    } else {
-        document.querySelector('.index').classList.add('active');
+    } else if (currentPage === "" && linkClass === "index") {
+        console.log("in home");
+        link.querySelector('a').classList.add('active');
     }
 });
